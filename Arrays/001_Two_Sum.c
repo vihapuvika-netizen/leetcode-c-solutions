b5ddd2ea055db338c3d11d7f2ec68346171a0e1f
Viha Puvikha <vihapuvikhavs2006@gmail.com>
2026-06-01 14:04:04 +0530

    Initial commit - Two Sum solution

---
+++ Arrays/001_Two_Sum.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int* ans = (int*)malloc(2 * sizeof(int));
+
+    for(int i=0; i<numsSize; i++)
+    {
+        for(int j=i+1; j<numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                ans[0] = i;
+                ans[1] = j;
+
+                *returnSize = 2;
+
+                return ans;
+            }
+        }
+    }
+
+    return NULL;
+}
